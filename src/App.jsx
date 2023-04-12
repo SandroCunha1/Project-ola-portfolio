e7ffd56fbbf45881838c3c7af3cc06fc82e80721
Sandro <sandromedeirosdacunha@gmail.com>
2023-04-12 16:23:38 -0300

    Instalando react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {BrowserRouter} from "react-router-dom"
+import AboutMe from "./paginas/aboutme"
+import Home from "./paginas/home"
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    
+    </BrowserRouter>
+  );
+}
+
+export default App
